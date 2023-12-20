a09998e562461441cd8ed0852c6ce7c3dd5c113b
John Palacios <johnpalacios.t@gmail.com>
2023-12-20 10:03:52 -0500

    Add Processor and Customer validations.

--- app/models/processor.rb
+++ app/models/processor.rb
@@ -1,3 +1,9 @@
 class Processor < ApplicationRecord
   has_many :customers
+
+  validates :cedula, presence: true, uniqueness: { case_sensitive: false }
+  validates :nombres, presence: true
+  validates :apellidos, presence: true
+  validates :celular, presence: true
+  validates :active, inclusion: { in: [true, false] }
 end
