49891c02504c9fa9281000f53d640f22e8970c97
John Palacios <johnpalacios.t@gmail.com>
2023-12-20 09:34:45 -0500

    Remove Role.

--- app/models/role.rb
+++
@@ -1,7 +0,0 @@
-class Role < ApplicationRecord
-  has_many :clientes
-
-  validates :nombre, presence: true, uniqueness: { case_sensitive: false }
-  validates :slug, presence: true, uniqueness: { case_sensitive: false }
-  validates :active, inclusion: { in: [true, false] }
-end
--- db/migrate/20231220141659_add_role_reference_to_clientes.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleReferenceToClientes < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :clientes, :role, foreign_key: true
-  end
-end
