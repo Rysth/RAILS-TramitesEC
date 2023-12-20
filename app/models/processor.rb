5deedd32356ba7ef3a5b45e4128ef903e6e568eb
John Palacios <johnpalacios.t@gmail.com>
2023-12-20 15:49:21 -0500

    Improve validations for processor.

--- app/models/processor.rb
+++ app/models/processor.rb
@@ -2,7 +2,7 @@ class Processor < ApplicationRecord
   belongs_to :user
   has_many :customers
 
-  validates :cedula, presence: true, uniqueness: { case_sensitive: false }
+  validates :cedula, presence: true, uniqueness: { case_sensitive: false }, length: { is: 10 }
   validates :nombres, presence: true
   validates :apellidos, presence: true
   validates :celular, presence: true
