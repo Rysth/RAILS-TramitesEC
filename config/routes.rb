a89eac2ecbf8ad9139d0da2aa9afcd22235ceacb
John Palacios <johnpalacios.t@gmail.com>
2023-12-20 10:04:51 -0500

    Update Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :user, only: [:show]  
-      resources :clientes
+      resources :processors
+      resources :customers
     end
   end
 end
