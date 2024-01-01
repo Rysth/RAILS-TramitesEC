711bce41fddf42badaab15c6bf7d4ffd12946cc6
John Palacios <johnpalacios.t@gmail.com>
2024-01-01 13:45:05 -0500

    Create StatusesController.

---
+++ app/controllers/api/v1/statuses_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::StatusesController < ApplicationController
+  before_action :authenticate_devise_api_token!
+
+  def index
+    @statuses = Status.all
+    render json: @statuses, status: :ok
+  end
+end
