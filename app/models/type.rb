4104a31975eea0f8794412ac76c253b7b68b5a2c
John Palacios <johnpalacios.t@gmail.com>
2024-01-01 13:08:02 -0500

    Generate License model.

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,4 @@
 class Type < ApplicationRecord
+  has_many :licenses
   validates :nombre, presence: true, uniqueness: { case_sensitive: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_01_180054) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_01_180319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_01_180054) do
     t.index ["previous_refresh_token"], name: "index_devise_api_tokens_on_previous_refresh_token"
     t.index ["refresh_token"], name: "index_devise_api_tokens_on_refresh_token"
     t.index ["resource_owner_type", "resource_owner_id"], name: "index_devise_api_tokens_on_resource_owner"
+  end
+
+  create_table "licenses", force: :cascade do |t|
+    t.string "nombre"
+    t.boolean "active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "type_id"
+    t.index ["type_id"], name: "index_licenses_on_type_id"
   end
 
   create_table "processors", force: :cascade do |t|
@@ -78,5 +87,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_01_180054) do
   end
 
   add_foreign_key "customers", "processors"
+  add_foreign_key "licenses", "types"
   add_foreign_key "processors", "users"
 end
