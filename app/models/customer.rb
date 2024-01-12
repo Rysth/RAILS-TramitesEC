c2a1b6cc1a18a4198a1c7b2581d6c052b4d8984b
John Palacios <johnpalacios.t@gmail.com>
2024-01-12 16:13:06 -0500

    Remove Direccion as needed for Customers.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,7 +7,6 @@ class Customer < ApplicationRecord
   validates :nombres, presence: true
   validates :apellidos, presence: true
   validates :celular, presence: true
-  validates :direccion, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :active, inclusion: { in: [true, false] }
 end
