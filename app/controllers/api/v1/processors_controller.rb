2d905167ad4299bd94c9a4f89ed3589cea3eb1cb
Rysth <johnpalacios.t@gmail.com>
2024-12-30 19:47:00 -0500

    Update processos_controller to uppercase names and remove whitepsace

--- app/controllers/api/v1/processors_controller.rb
+++ app/controllers/api/v1/processors_controller.rb
@@ -23,7 +23,7 @@ class Api::V1::ProcessorsController < ApplicationController
     total_pages = procedures.total_pages
 
     render json: {
-      procedures: procedures.as_json(include: { 
+      procedures: procedures.as_json(include: {
                                        customer: { only: %i[id first_name last_name] },
                                        status: { only: %i[id name] },
                                        procedure_type: { only: :name },
@@ -81,7 +81,8 @@ class Api::V1::ProcessorsController < ApplicationController
     processors = if query.blank?
                    Processor.order(created_at: :desc).page(1)
                  else
-                   Processor.where('LOWER(code) LIKE :query OR LOWER(CONCAT(first_name, \' \', last_name)) LIKE :query', query: "%#{query}%").order(created_at: :desc).page(1)
+                   Processor.where('LOWER(code) LIKE :query OR LOWER(CONCAT(first_name, \' \', last_name)) LIKE :query',
+                                   query: "%#{query}%").order(created_at: :desc).page(1)
                  end
     render json: processors.as_json(only: %i[id code first_name last_name])
   end
@@ -90,20 +91,18 @@ class Api::V1::ProcessorsController < ApplicationController
     # Extract start_date and end_date from params
     start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : nil
     end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : nil
-  
+
     # Query processors within the specified date range and order by created_at in ascending order
     processors = Processor.includes(:user)
-    
+
     # Apply date range filtering if dates are provided
-    if start_date && end_date
-      processors = processors.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
-    end
-  
+    processors = processors.where(created_at: start_date.beginning_of_day..end_date.end_of_day) if start_date && end_date
+
     # Order processors by created_at in ascending order
     processors = processors.order(created_at: :asc)
-  
+
     is_admin = params[:is_admin] == 'true' if params[:is_admin].present?
-  
+
     # Generate Excel file using axlsx_rails gem
     package = Axlsx::Package.new
     workbook = package.workbook
@@ -112,40 +111,40 @@ class Api::V1::ProcessorsController < ApplicationController
       header_rows = ['ID', 'Usuario', 'Código', 'Nombres', 'Apellidos', 'Teléfono', 'Fecha de Creación', 'Total de Clientes', 'Total de Trámites']
       header_rows.push('Total de Valores', 'Total de Ganancias') if is_admin
       sheet.add_row header_rows
-  
+
       # Add data for each processor
       total_clients_all = 0
       total_procedures_all = 0
       total_cost_all = 0
       total_profit_all = 0
-  
+
       processors.each do |processor|
         # Calculate total values for the current processor
         has_procedures = processor.procedures.count.positive?
-  
+
         total_clients = processor.customers_count
         total_procedures = processor.procedures_count
         total_cost = processor.procedures.sum(:cost) if has_procedures
         total_profit = processor.procedures.sum(:profit) if has_procedures
-  
+
         total_clients_all += total_clients
         total_procedures_all += total_procedures
         total_cost_all += total_cost if has_procedures
         total_profit_all += total_profit if has_procedures
-  
+
         body_rows = [processor.id, processor.user.username, processor.code, processor.first_name, processor.last_name, processor.phone,
                      processor.created_at, total_clients, total_procedures]
         body_rows.push(total_cost, total_profit) if is_admin
-  
+
         sheet.add_row body_rows
       end
-  
+
       # Add totals row
       totals_row = ['Totales', '', '', '', '', '', '', total_clients_all, total_procedures_all]
       totals_row.push(total_cost_all, total_profit_all) if is_admin
       sheet.add_row totals_row
     end
-  
+
     # Set the content type for the response and send the file
     send_data package.to_stream.read, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename: 'tramitadores.xlsx'
   end
@@ -213,8 +212,6 @@ class Api::V1::ProcessorsController < ApplicationController
     processors.each do |supplier|
       puts supplier.inspect
     end
-  
-  
 
     processors.page(params[:page]).per(15)
   end
@@ -235,6 +232,9 @@ class Api::V1::ProcessorsController < ApplicationController
   end
 
   def processor_params
-    params.require(:processor).permit(:id, :code, :first_name, :last_name, :phone)
+    params.require(:processor).permit(:id, :code, :first_name, :last_name, :phone).tap do |processor_params|
+      processor_params[:first_name] = processor_params[:first_name].strip.upcase if processor_params[:first_name].present?
+      processor_params[:last_name] = processor_params[:last_name].strip.upcase if processor_params[:last_name].present?
+    end
   end
 end
