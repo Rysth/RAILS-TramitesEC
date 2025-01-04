c1aae19df3b12341798452571ea68cfa15b69486
Rysth <johnpalacios.t@gmail.com>
2025-01-04 12:18:43 -0500

    Solve problem with selectedYear

--- app/controllers/api/v1/procedures_controller.rb
+++ app/controllers/api/v1/procedures_controller.rb
@@ -124,7 +124,7 @@ class Api::V1::ProceduresController < ApplicationController
   end
 
   def all_procedures
-    procedures = Procedure.includes(:user, :customer, :processor, :procedure_type, :license, :status).order(id: :desc)
+    procedures = Procedure.includes(:user, :customer, :processor, :procedure_type, :license, :status, :supplier).order(id: :desc)
 
     if params[:search].present?
       search_term = "%#{params[:search].downcase}%"
